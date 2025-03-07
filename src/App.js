122ea3f5332ca93246fa7e342e726807a093fb68
NainaPawale <142329265+Nainapawale@users.noreply.github.com>
2025-03-07 18:13:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,11 @@ import Hero from "./Components/Hero/Hero";
 import Footer from "./Components/Footer/Footer";
 
 const App = () => {
-  let heroData = [
-    { text1: "What is right? ", text2: "What you want?" },
-    { text1: "Indulge", text2: "your passion" },
-    { text1: "Drive on", text2: "your goals" },
-  ];
-
-  const [heroCount, setHeroCount] = useState(0);
-  // const [playStatus,setPlayStatus]=useState(false);
-
   return (
     <div>
-      <Background heroCount={heroCount} />
+      <Background />
       <Navbar />
-      <Hero
-        heroData={heroData[heroCount]}
-        heroCount={heroCount}
-        setHeroCount={setHeroCount}
-      />
+      <Hero />
       <Footer />
     </div>
   );
